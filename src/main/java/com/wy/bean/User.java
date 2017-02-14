a0651c7aaa99699fba1afde52ce3810a05cebe5c
A Distance There Is <1517262320@qq.com>
2017-02-14 15:39:59 +0800

    添加项目主体

---
+++ src/main/java/com/wy/bean/User.java
@@ -0,0 +1,11 @@
+package com.wy.bean;
+
+/**
+ * Created by Administrator on 2/14/2017.
+ */
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+}
