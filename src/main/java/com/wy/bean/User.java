7ad70fa11553bbf3c52bb1e8f51b7f23060fd921
A Distance There Is <1517262320@qq.com>
2017-02-15 09:47:11 +0800

    集成三大框架之间的配置，增加log4j日志。增加所相应的页面和后台对应的bean，service，dao，controller

--- src/main/java/com/wy/bean/User.java
+++ src/main/java/com/wy/bean/User.java
@@ -1,6 +1,7 @@
 package com.wy.bean;
 
 /**
+ * 普通用户
  * Created by Administrator on 2/14/2017.
  */
 public class User {
@@ -8,4 +9,66 @@ public class User {
     private String password;
     private String email;
     private String address;
+    private int phone;
+    private String headicon;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getHeadicon() {
+        return headicon;
+    }
+
+    public void setHeadicon(String headicon) {
+        this.headicon = headicon;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", phone=" + phone +
+                ", headicon='" + headicon + '\'' +
+                '}';
+    }
 }
